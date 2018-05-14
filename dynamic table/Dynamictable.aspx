d783eeed8c1a6b887062e6ce6aeb159d50b2c242
Priyanka <33025640+priyanka2907@users.noreply.github.com>
2018-05-14 10:46:04 +0530

    Add files via upload

---
+++ dynamic table/Dynamictable.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Dynamictable.aspx.vb" Inherits="Learn_Project.Dynamictable" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Create Dynamic table</title>
+
+    <link href="../css/custom.css" rel="stylesheet" />
+    <link href="../css/bootstrap.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <h3>Dynamic Table Example</h3>
+
+    <div class="row">
+
+    
+    <div class="col-sm-3"> </div>
+      <div class="col-sm-6"> 
+    <div id="tbl" runat="server">
+    
+
+
+
+    </div ></div>
+      <div class="col-sm-3"> </div>
+    </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ dynamic table/Dynamictable.aspx.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Data
+Imports System.IO
+
+
+Public Class Dynamictable
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'assign value to div
+
+        tbl.InnerHtml += "<table class='table table-bordered'>"
+        'add table header
+        For i = 1 To 5
+
+
+            tbl.InnerHtml += "<thaed><th>Test " & i & "</th> </thead>"
+
+        Next
+
+        'start new row
+        tbl.InnerHtml += "<tr>"
+        'add row data
+        For j = 1 To 10 'for rowcount
+
+            For k = 1 To 5 'for row data
+                tbl.InnerHtml += "<td> <input type='text' class='form-control' name='text" & j & "' class='txt'></td>"
+            Next
+            tbl.InnerHtml += "</tr>"
+        Next
+        tbl.InnerHtml += "</table>"
+    End Sub
+
+End Class
